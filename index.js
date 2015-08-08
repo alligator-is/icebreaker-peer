715a6a189e5fdc1494b21e936d71e637e5d8c4c5
alligator-io <markus@sonium.org>
2015-08-09 01:34:35 +0200

    breaking changes: using module.exports instead of _.mixin

--- index.js
+++ index.js
@@ -8,144 +8,144 @@ function isFunction(obj) {
   return typeof obj === 'function'
 }
 
-_.mixin({
-  peer: function (params) {
-    return function (options) {
-      var emitter = new EventEmitter()
-
-      emitter.connections = {}
-
-      function check(listener){
-        if (this.listeners(listener).length === 0) {
-          throw new Error('no peer '+listener+' listener defined')
-        }
-      }
-
-      function apply(options) {
-        if (typeof options === 'object') {
-          for (var k in options) {
-            var value = options[k]
-            if (!isFunction(value)) emitter[k] = value
-          }
-        }
-      }
-
-      emitter.start = function (options) {
-        apply(options)
-        if (!this.address) this.address = ip.address()
-        if (!this.port) this.port = 6005
-
-        check.call(this,'start')
-        this.emit('start')
-      }
-
-      var seen =[]
-
-      emitter.stop = function () {
-        var self = this
-
-        check.call(this, 'stop')
-
-        _(
-          [ this.connections ],
-          _.flatten(),
-          _.filter(function (c) {
-            return seen.indexOf(c.id) === -1 && isFunction(c.close)
-          }),
-          _.asyncMap(function (c, cb) {
-            seen.push(c.id)
-            c.close(function () { cb() })
-           }),
-          _.onEnd(function () {
-            setTimeout(function () {
-              if (self.connections && Object.keys(self.connections).length > 0)
-                return self.stop()
-              seen = []
-              self.emit('stop')
-            }, 50)
-           })
-         )
-     }
-
-      emitter.connect = function (options) {
-        check.call(this,'connect')
-        if (typeof options !== 'object') options = {}
-        options.direction=1
-        if(typeof options.address ==='string' && !net.isIP(options.address)){
-          options.hostname = options.address
-        }
-        this.emit('connect', options)
-      }
-
-      emitter.connection = function (params) {
-        if (!params.type) params.type = this.name
-        if (!params.id) params.id = uuid.v4()
-        if (!params.address) params.address = this.address || ip.address()
-        if (!params.port) params.port = this.port
-        if (!params.direction)params.direction=-1
-
-        var source = params.source, sink = params.sink
-        var ended=false,_ended=false
-        var self = this
-        var del = function(){
-          if(ended===true && _ended === true){
-            var connection = self.connections[params.id]
-            delete self.connections[params.id]
-            del=null
-            params=null
-            self.emit('disconnected',connection)
-          }
-        }
-
-        params.source = function(end,cb){
-         source(end,function(end,data){
-          if(end)ended=true
-          cb(end,data)
-          if(end&&del)del()
-          })
-        }
-
-        params.sink = function(read){
-         sink(function(abort,cb){
-           if(abort) {
-             _ended=true
-              if(del)del()
-              return read(abort,cb)
-           }
-           read(abort,function(end,data){
-            cb(end,data)
-            if(end)_ended=true
-            if(end&&del)del()
-          })
-         })
-        }
-
-        emitter.connections[params.id] = params
-        this.emit('connection', params)
-      }
-
-      apply(params)
-      apply(options)
-
-      for (var key in params) {
-        var value = params[key]
-        if (!isFunction(value)) this[key] = params[key]
-      }
-
-      var handle = function (key) {
-        if (isFunction(params[key])) {
-          var event = function () {
-            params[key].apply(this, [].slice.call(arguments))
-          }.bind(this)
-          this.on(key, event)
-        }
-      }.bind(emitter)
-
-      handle('start')
-      handle('stop')
-      handle('connect')
-
-      return emitter
+function apply(options, to) {
+  if (typeof options === 'object') {
+    for (var k in options) {
+      var value = options[k]
+      if (!isFunction(value)) to[k] = value
     }
   }
-})
+}
+
+function check(emitter, listener) {
+  if (emitter.listeners(listener).length === 0) {
+    throw new Error('no peer ' + listener + ' listener defined')
+  }
+}
+
+function Peer(params,ext) {
+  if(!(this instanceof Peer)) return new Peer(params,ext)
+
+  EventEmitter.call(this)
+
+  if(ext)apply(ext,params)
+
+  this.connections = {}
+  this._seen = []
+
+  apply(params, this)
+
+  this.address = this.address||ip.address()
+
+  this.port = this.port||6005
+
+  if (isFunction(params.start)) {
+    this.on('start', params.start)
+  }
+
+  if (isFunction(params.stop)) {
+    this.on('stop', params.stop)
+  }
+
+  if (isFunction(params.connect)) this.on('connect', params.connect)
+}
+
+var proto = Peer.prototype = Object.create(EventEmitter.prototype)
+proto.constructor = Peer
+
+proto.start = function (options) {
+  apply(options, this)
+  check(this, 'start')
+  this.emit('start')
+}
+
+proto.stop = function () {
+  var self = this
+
+  check(this, 'stop')
+
+  _(
+    [this.connections],
+    _.flatten(),
+    _.filter(function (c) {
+      return self._seen.indexOf(c.id) === -1 && isFunction(c.close)
+    }),
+    _.asyncMap(function (c, cb) {
+      self._seen.push(c.id)
+      c.close(function () {
+        cb()
+      })
+    }),
+    _.onEnd(function () {
+      setTimeout(function () {
+        if (self.connections && Object.keys(self.connections).length > 0)
+          return self.stop()
+        self._seen = []
+        self.emit('stop')
+      }, 50)
+    })
+  )
+}
+
+proto.connect = function (params) {
+  check(this, 'connect')
+  if (typeof params !== 'object') params = {}
+  params.direction = 1
+  if (typeof params.address === 'string' && !net.isIP(params.address)) {
+    params.hostname = params.address
+  }
+  if (!params.id)params.id = uuid.v4()
+
+  this.emit('connect', params)
+}
+
+
+proto.connection = function (params) {
+  var self = this
+
+  if (!params.type) params.type = this.name
+  if (!params.id)params.id = uuid.v4()
+  if (!params.address) params.address = this.address || ip.address()
+  if (!params.port) params.port = this.port
+  if (!params.direction) params.direction = -1
+  if(params.direction === -1)params.peer = self.peer
+
+  var source = params.source, sink = params.sink
+  var ended = false, _ended = false
+  var del = function () {
+    if (ended === true && _ended === true) {
+      var connection = self.connections[params.id]
+      delete self.connections[params.id]
+      del = null
+      params = null
+      self.emit('disconnected', connection)
+    }
+  }
+
+  params.source = function (end, cb) {
+    source(end, function (end, data) {
+      if (end) ended = true
+      cb(end, data)
+      if (end && del) del()
+    })
+  }
+
+  params.sink = function (read) {
+    sink(function (abort, cb) {
+      if (abort) {
+        _ended = true
+        if (del) del()
+        return read(abort, cb)
+      }
+      read(abort, function (end, data) {
+        cb(end, data)
+        if (end) _ended = true
+        if (end && del) del()
+      })
+    })
+  }
+
+  this.emit('connection', this.connections[params.id] = params)
+}
+
+module.exports = Peer
