db78f636cce3a59025ac1657c4fbd3000a1b8267
alligator-io <markus@sonium.org>
2017-12-06 23:38:26 +0100

    more tests

--- test.js
+++ test.js
@@ -5,35 +5,56 @@ var cl = require('chloride')
 var Peer = require('./')
 
 function authenticate(id, cb) {
-    cb(null, true)
+  cb(null, true)
 }
 
 var alice = cl.crypto_sign_keypair()
 var peer = Peer({ keys: alice, authenticate: authenticate, appKey: 'alligator@1.0.0' })
 peer.listen('shs+tcp://localhost:9090')
 
-test('shs+tcp',function(t){
-t.plan(4)
-_(
+test('shs+tcp', function (t) {
+  t.plan(4)
+  _(
     peer,
     peer.on({
-        ready: function (e) {
-          console.log(e)
-            t.equal(e.address.length,1)
-            peer.connect(e.address[0])
-        },
-        connection: function (e) {
-            _(['hello world'], e, _.drain(function (d) {
-              t.equal('hello world',d.toString())
-            }, function () {
-                peer.end()
-            }))
-        },
-        end: t.notOk
+      ready: function (e) {
+        t.equal(e.address.length, 1)
+        peer.connect(e.address[0])
+      },
+      connection: function (e) {
+        _(['hello world'], e, _.drain(function (d) {
+          t.equal('hello world', d.toString())
+        }, function () {
+          peer.end()
+        }))
+      },
+      end: t.notOk
     })
   )
+
+
 })
 
-process.on('SIGINT', function(){
-    peer.end();
+test('shs+tcp+unix', function (t) {
+  peer = Peer({ keys: alice, authenticate: authenticate, appKey: 'alligator@1.0.0' })
+  peer.listen('shs+tcp+unix:///C/Users/Markus/AppData/Local/Temp/alligator.sock')
+
+  t.plan(4)
+  _(
+    peer,
+    peer.on({
+      ready: function (e) {
+        t.equal(e.address.length, 1)
+        peer.connect(e.address[0])
+      },
+      connection: function (e) {
+        _(['hello world'], e, _.drain(function (d) {
+          t.equal('hello world', d.toString())
+        }, function () {
+          peer.end()
+        }))
+      },
+      end: t.notOk
+    })
+  )
 })
