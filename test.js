ccf71e9d965a335130f62bcbe5d7fbf7f596edd0
alligator-io <markus@sonium.org>
2018-10-21 17:03:30 +0200

    es6 syntax

--- test.js
+++ test.js
@@ -1,43 +1,40 @@
-var test = require('tape')
-var _ = require('icebreaker')
-var Peer = require('./')
-var cl = require('chloride')
-var Peer = require('./')
-var os = require('os')
-var path = require('path')
+const test = require('tape')
+const _ = require('icebreaker')
+const Peer = require('./')
+const cl = require('chloride')
+const os = require('os')
+const path = require('path')
 
 function authenticate(id, cb) {
   cb(null, true)
 }
 
-var alice = cl.crypto_sign_keypair()
-var peer = Peer({ keys: alice, authenticate: authenticate, appKey: 'alligator@1.0.0' ,encoding:"base58"})
+const alice = cl.crypto_sign_keypair()
+let peer = Peer({ keys: alice, authenticate: authenticate, appKey: 'alligator@1.0.0' ,encoding:"base58"})
 peer.listen('shs+tcp://localhost:9090')
 
-test('shs+tcp', function (t) {
+test('shs+tcp', (t) =>{
   t.plan(4)
   _(
     peer,
     peer.on({
-      ready: function (e) { 
+      ready:(e)=>{ 
         t.equal(e.address.length, 1)
         peer.connect(e.address[0])
       },
-      connection: function (e) {
+      connection:(e) =>{
         _(['hello world'], e, _.drain(function (d) {
           t.equal('hello world', d.toString())
-        }, function () {
-          peer.end()
-        }))
+        }, 
+        peer.end
+        ))
       },
       end: t.notOk
     })
   )
-
-
 })
 
-test('shs+tcp+unix', function (t) {
+test('shs+tcp+unix', (t) => {
 
   peer = Peer({ keys: alice, authenticate: authenticate, appKey: 'alligator@1.0.0' })
   peer.listen('shs+tcp+unix://'+path.join("/",os.tmpdir() , '/test4.socket'))
@@ -46,11 +43,11 @@ test('shs+tcp+unix', function (t) {
   _(
     peer,
     peer.on({
-      ready: function (e) {
+      ready: (e) => {
         t.equal(e.address.length, 1)
         peer.connect(e.address[0])
       },
-      connection: function (e) {
+      connection:(e) => {
         _(['hello world'], e, _.drain(function (d) {
           t.equal('hello world', d.toString())
         }, function () {
